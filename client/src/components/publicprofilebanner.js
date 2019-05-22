80cde6274e273c3e7aee8fb87a8e455474d7d220
Anthony Salemo <ajssalemo@gmail.com>
2019-05-21 21:45:36 -0400

    refactored a query and passed down props - removing two uneeded queries

--- client/src/components/publicprofilebanner.js
+++ client/src/components/publicprofilebanner.js
@@ -2,19 +2,14 @@
 // ----------------------------------------------------------------------------------------------------- //
 
 import React from 'react';
-import { Query } from 'react-apollo';
 
 // Material-UI components
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
-// Apollo Queries
-import { GET_USERPROFILE_TWEETS } from '../apolloclient/apolloqueries';
-
 // Components
 import Error from '../components/error';
-
 
 // ----------------------------------------------------------------------------------------------------- //
 // ----------------------------------------------------------------------------------------------------- //
@@ -41,35 +36,23 @@ const styles = () => ({
 // ----------------------------------------------------------------------------------------------------- //
 
 const PublicProfileBanner = props => {
-    const { classes, URLparam } = props;
+    const { classes, error, loading, profileBannerURL } = props;
     return (
-        <Query 
-            query={GET_USERPROFILE_TWEETS}
-            fetchPolicy='cache-and-network'
-            variables={{
-                screen_name: URLparam
-            }}
-        >
-            {({ loading, error, data }) => {
-                console.log(data)
-                if (loading) return <div className={classes.bannerErrorAndLoadingDiv}><CircularProgress /></div>;
-                if (error) return <div className={classes.bannerErrorAndLoadingDiv}><Error /></div>;
-
-                return (
-                    data.currentUser.userProfileTweets[0].user.profile_banner_url
-                        ?
-                    <Grid item className={classes.bannerHeight}>
-                        <img 
-                            src={data.currentUser.userProfileTweets[0].user.profile_banner_url} 
-                            className={classes.bannerImage}
-                            alt='User profile banner' 
-                        />
-                    </Grid>
-                        :
-                    <Grid item className={classes.bannerPlaceholderImage}></Grid>
-                );
-            }}
-        </Query>
+        loading ? <div className={classes.bannerErrorAndLoadingDiv}><CircularProgress /></div>
+            :
+        error ? <div className={classes.bannerErrorAndLoadingDiv}><Error /></div>  
+            :   
+        profileBannerURL
+            ?
+        <Grid item className={classes.bannerHeight}>
+            <img 
+                src={profileBannerURL}
+                className={classes.bannerImage}
+                alt='User profile banner' 
+            />
+        </Grid>
+            :
+        <Grid item className={classes.bannerPlaceholderImage}></Grid>
     );
 };
 
--- client/src/pages/main/publicprofile.js
+++ client/src/pages/main/publicprofile.js
@@ -2,12 +2,15 @@
 // ----------------------------------------------------------------------------------------------------- //
 
 import React from 'react';
+import { Query } from 'react-apollo';
 import { withRouter } from 'react-router';
 
 // Material-UI components
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 
+// Apollo Queries
+import { GET_USERPROFILE_TWEETS } from '../../apolloclient/apolloqueries';
 
 // Components
 import Navbar from '../../components/navbar';
@@ -37,25 +40,50 @@ let PublicProfile = props => {
     const param = props.match.params.params; 
     const { classes } = props;
     return (
-        <React.Fragment>
-            <Navbar /> 
-            <PublicProfileBanner
-                URLparam={param}
-            />
-            <ProfileBannerBar 
-                URLparam={param}
-            />
-            <Grid container className={classes.publicProfileContainerStyle}>
-                <Grid item xs={8} sm={8} md={2} className={classes.publicProfileHandlerGrid}>
-                    {/* This component is a place holder, for now */}
-                    <ProfileHandle />
-                </Grid>
-                <Grid item md={2}>
-                    <Recommended />
-                    <Trending />
-                </Grid>
-            </Grid>
-        </React.Fragment>
+        <Query 
+            query={GET_USERPROFILE_TWEETS}
+            fetchPolicy='cache-and-network'
+            variables={{
+                screen_name: param
+            }}
+        >
+            {({ loading, error, data }) => {
+                console.log(data)
+                return (
+                    <React.Fragment>
+                        <Navbar /> 
+                        <PublicProfileBanner
+                            loading={loading}
+                            error={error}
+                            profileBannerURL={data.currentUser.userProfileTweets[0].user.profile_banner_url}
+                        />
+                        <ProfileBannerBar 
+                            loading={loading}
+                            error={error}
+                            statuses_count={data.currentUser.userProfileTweets[0].user.statuses_count}
+                            friends_count={data.currentUser.userProfileTweets[0].user.friends_count}
+                            followers_count={data.currentUser.userProfileTweets[0].user.followers_count}
+                            favourites_count={data.currentUser.userProfileTweets[0].user.favourites_count}
+                            listed_count={data.currentUser.userProfileTweets[0].user.listed_count}
+                            screen_name={data.currentUser.userProfileTweets[0].user.screen_name}
+                            tweetUserId={data.currentUser.userProfileTweets[0].user.id}
+                            currentUser={data.currentUser.nickname}
+                            profileImageURL={data.currentUser.userProfileTweets[0].user.profile_image_url_https}
+                        />
+                        <Grid container className={classes.publicProfileContainerStyle}>
+                            <Grid item xs={8} sm={8} md={2} className={classes.publicProfileHandlerGrid}>
+                                {/* This component is a place holder, for now */}
+                                <ProfileHandle />
+                            </Grid>
+                            <Grid item md={2}>
+                                <Recommended />
+                                <Trending />
+                            </Grid>
+                        </Grid>       
+                    </React.Fragment>   
+                );
+            }}
+        </Query>
     );
 };
 
