7983f6d4f525fb1d3a7394c8617922006bebfe8f
Anthony Salemo <ajssalemo@gmail.com>
2019-08-19 21:10:47 -0400

    converted public followers page to be responsive

--- client/src/pages/main/profilefollowers.js
+++ client/src/pages/main/profilefollowers.js
@@ -29,23 +29,42 @@ import { changeGridBackground, fontColorChange, changeComponentBackground, chang
 // ----------------------------------------------------------------------------------------------------- //
 // ----------------------------------------------------------------------------------------------------- //
 
-const styles = () => ({
+const styles = theme => ({
     publicProfileFollowersContainerStyle: {
         paddingTop: '0.7em',
         display: 'flex',
         justifyContent: 'center'
     },
     publicProfileFollowersHandlerGrid: {
-        width: 'auto',
-        paddingRight: '2em',
-        marginLeft: '10em'
+        marginLeft: '0em',
+        [theme.breakpoints.up('md')]: {
+            width: 'auto',
+            paddingRight: '2em',
+            marginLeft: '10em'    
+        }
     },
     errorAndLoadingDiv: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh'
+    },
+    trendingProfileFollowersGrid: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'initial'
+        },
+    },
+    followersGrid: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        [theme.breakpoints.up('sm')]: {
+            display: 'initial',
+            marginLeft: '1em'
+        }
     }
+
 });
 
 // ----------------------------------------------------------------------------------------------------- //
@@ -99,7 +118,7 @@ let ProfileFollowers = props => {
                                     darkModeComponentBackground={changeComponentBackground(dark_mode)}        
                                 />
                                 <Grid container className={classes.publicProfileFollowersContainerStyle} style={{ backgroundColor: changeGridBackground(dark_mode) }}>
-                                    <Grid item xs={8} sm={8} md={2} className={classes.publicProfileFollowersHandlerGrid}>
+                                    <Grid item xs={8} sm={4} md={2} className={classes.publicProfileFollowersHandlerGrid}>
                                         <PublicProfileHandle 
                                             URLparam={param}
                                             currentUser={two.currentUser.verifyCredentials.screen_name}
@@ -112,14 +131,16 @@ let ProfileFollowers = props => {
                                             avatarImg={two.currentUser.verifyCredentials.profile_image_url_https}
                                             darkModeFont={fontColorChange(dark_mode)}        
                                         />
-                                        <Trending 
-                                            profileLinkColor={one.currentUser.showUser.profile_link_color}
-                                            darkModeBorder={changeBorder(dark_mode)}
-                                            darkModeFont={fontColorChange(dark_mode)}
-                                            darkModeComponentBackground={changeComponentBackground(dark_mode)}        
-                                        />
+                                        <Grid item className={classes.trendingProfileFollowersGrid}>
+                                            <Trending 
+                                                profileLinkColor={one.currentUser.showUser.profile_link_color}
+                                                darkModeBorder={changeBorder(dark_mode)}
+                                                darkModeFont={fontColorChange(dark_mode)}
+                                                darkModeComponentBackground={changeComponentBackground(dark_mode)}        
+                                            />
+                                        </Grid>
                                     </Grid>
-                                    <Grid item md={7}>
+                                    <Grid item md={7} sm={6} className={classes.followersGrid}>
                                         <UsersFollowersList
                                             currentUser={two.currentUser.verifyCredentials.screen_name}
                                             screenName={one.currentUser.showUser.screen_name}
