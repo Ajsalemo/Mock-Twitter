6df6cbfc0573e0b5c30efdf05debd020ad731631
Anthony Salemo <ajssalemo@gmail.com>
2019-08-26 20:38:24 -0400

    deploying

---
+++ client/build/precache-manifest.dc98bf2fa68d27a5f82a14defc5f8f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83bc3b53c13c568bb05b",
+    "url": "/static/js/main.83bc3b53.chunk.js"
+  },
+  {
+    "revision": "f84070f50261e0103211",
+    "url": "/static/js/1.f84070f5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d74e8bfd4ef7985f7529bb9f7650eca",
+    "url": "/static/media/twitterminilogo.3d74e8bf.png"
+  },
+  {
+    "revision": "87dfb9b35a216fdbe30306076d53a70e",
+    "url": "/static/media/twitterlogo.87dfb9b3.jpg"
+  },
+  {
+    "revision": "5712048d1e8fea4ed22ca78353a27883",
+    "url": "/index.html"
+  }
+];
